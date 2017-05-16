8acafc6255822e2a7749d238a97a23702e84c803
Elmer Mora <elmermora@MacBook-Pro.local>
2017-05-16 07:38:21 -0500

    First commit for a POC on Selenium using python

---
+++ test_comparamejor_quote.py
@@ -0,0 +1,33 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://comparamejor.com/")
+
+    def test_quotation_in_funnel(self):
+        driver = self.driver
+        self.assertIn("Cotizar seguro para vehículo y compra SOAT | ComparaMejor.com", driver.title)
+        quote_button = driver.find_element_by_id("quote-button")
+        self.assertTrue(quote_button.is_displayed(), "Quote button is not visible")
+        car_id = driver.find_element_by_id("quote")
+        car_id.send_keys("ABC123")
+        car_id.send_keys(Keys.RETURN)
+        wait = WebDriverWait(driver, 20)
+        cmuj_selector = self.driver.find_element_by_id("uj40")
+        self.assertTrue(cmuj_selector.is_displayed(), "Car type selector is not visible")
+
+    def test_filling_funnel(self):
+        driver = self.driver
+        
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
